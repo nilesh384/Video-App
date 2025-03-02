9eff2ee6b07c35b6035d64c50eebbe335b091d54
Nilesh Bera <nileshbera258@gmail.com>
2025-03-03 01:19:39 +0530

    connected mongoDB Cluster

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// require('dotenv').config({path: "./.env"})      reduces consistency of the code as require and import both statements are used
+
+import dotenv from"dotenv";                         // dotenv is a package to read the .env file and set the environment variables
+
+import express from "express";
+import dbConn from "./databases/dbConn.js";
+
+const app = express();
+
+dotenv.config({ path: "./.env" });                 // dotenv is a package to read the .env file and set the environment variables
+
+dbConn();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Approach
+/*
+( async () => {
+
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Connected to DB")
+        app.on("close", (error) => {
+            console.log("Server closed")
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port ${process.env.PORT}`)
+        })
+
+    }catch(e){
+        console.error("ERROR: ", e)
+        throw e
+    }
+
+} )()
+
+*/
+
+// Second approach is writing on a seperate file and importing it here
