77b4043eaae59a1e793c1747572a32436c2e06f7
Nilesh Bera <nileshbera258@gmail.com>
2025-07-30 22:24:03 +0530

    CORS upadte

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || ["*"];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials : true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
