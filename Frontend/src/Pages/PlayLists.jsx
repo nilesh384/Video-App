b1481b7a0969b742775a990ab9ee4c062abfa1e9
Nilesh Bera <nileshbera258@gmail.com>
2025-07-30 18:56:43 +0530

    update for deploy

--- Frontend/src/Pages/PlayLists.jsx
+++ Frontend/src/Pages/PlayLists.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaPlus, FaTrash } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
 
-const PlayList= () => {
+const PlayLists= () => {
   const { videoId } = useParams();
   const [playlists, setPlaylists] = useState([]);
   const [selectedPlaylistId, setSelectedPlaylistId] = useState(null);
@@ -299,4 +299,4 @@ const PlayList= () => {
   );
 };
 
-export default PlayList;
+export default PlayLists;
