1c717a89f6bc6556886b1f10c3f082eba5be18ea
Nilesh Bera <nileshbera258@gmail.com>
2025-07-31 15:31:35 +0530

    up

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed  by CORS"));
     }
   },
   credentials: true
